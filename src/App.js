c7fe5d8b209305f49bb7f8a168657dadfbfbd717
unknown <romanyefimets@gmail.com>
2022-02-23 09:43:41 -0800

    items can be removed from list by clicking trash icon

--- src/App.js
+++ src/App.js
@@ -6,24 +6,44 @@ const App = () => {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
+      showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // deal with edit
     } else {
-      // show alert
+      showAlert(true, 'success', 'item added to the list');
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName('');
     }
   };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+
   return (
     <section className="section-center">
       <form onSubmit={handleSubmit} className="grocery-form">
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -40,8 +60,10 @@ const App = () => {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
--- src/alert.component.jsx
+++ src/alert.component.jsx
@@ -1,5 +1,13 @@
-const Alert = () => {
-  return <div>alert</div>;
+import { useEffect } from 'react';
+
+const Alert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  });
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
 export default Alert;
